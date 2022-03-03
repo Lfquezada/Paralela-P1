6ed78628e72cbf33a794068111be60c96a4dda03
JennsiS <42972809+JennsiS@users.noreply.github.com>
2022-03-03 16:49:06 -0600

    Update PDESeq.c

--- PDESeq.c
+++ PDESeq.c
@@ -6,17 +6,21 @@
 // gcc PDESeq.c -o PDESeq
 // ./PDESeq 0.0001 5 30 10 100
 
-#define n 10
+//#define n 10
+#define N 10
+#define L 50
 
 int main(int argc, char* argv[]) {
   double err, t0, tl, tr;
-  int N=n;
+  //int N=n;
+  //const int N = 10;
   double c = pow(10, -5);
+  //const int L = 50;
   double vec_sol;
 
   if(argc > 1) {
     err = strtol(argv[1], NULL, 10); // Valor de error
-    N = strtol(argv[2], NULL, 10); // Numero de intervalo discretos
+    //N = strtol(argv[2], NULL, 10); // Numero de intervalo discretos
     t0 = strtol(argv[3], NULL, 10); // Temperatura inicial
     tl = strtol(argv[4], NULL, 10); // Temperatura de la frontera izquierda
     tr = strtol(argv[5], NULL, 10); // Temperatura de la frontera derecha
@@ -24,22 +28,30 @@ int main(int argc, char* argv[]) {
 
   printf("VALORES: %f %d %f %f %F\n", err,N,t0,tl,tr);
 
+
   // 1. División del dominio en intervalos discretos (tiempo y distancia)
+
+  const int intervalos = L/N;
 
   // 2. Ajustar los valores iniciales de los vectores de solución Ti y Ti+1, para tiempo i
   //double Ti_p1[N] = { 0 };
 
-  double Ti[N];
+  double Ti[N] = {t0};
+  double Ti_p1[N] = {0};
+
+  //double Ti[N] = {t0};
   memcpy(Ti, (int[]){ t0 }, N*sizeof(int));
 
-  for(int i=0; i<N; i++) {
-    printf("%f ", Ti[i]);
-  }
+  
 
   // 3. For (resultado en vec_sol)
+
   while (err > (1000)) {
     for (int j = 1; j <= N-1; j++) {
-      // Calcular Tj(ti_p1)
+      double Ti_p1[N] = {0};  //Cada iteracion de tiempo reiniciar el vector de resultados en 0
+      for (int i=0; i<N; i++){
+        //Calculo de las temperaturas
+      }
     }
   }
 
